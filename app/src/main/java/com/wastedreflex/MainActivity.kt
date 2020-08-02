4bf52c9ae370332d6196be3428ce59f9b1f6f8d6
MaKiVerem4011 <gergo.tallai@gmail.com>
2020-08-02 16:25:45 +0200

    Banner reklám TestID-val működik. AndroidManifest.xml-be és a layout-on kell az ID változtatni és lehet tesztelni élesbe telefonon

--- app/src/main/java/com/wastedreflex/MainActivity.kt
+++ app/src/main/java/com/wastedreflex/MainActivity.kt
@@ -2,13 +2,22 @@ package com.wastedreflex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mAdView : AdView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-    fun geci(){
-        val anyad = 10
+//AdMob: Initialize Mobile Ads SDK
+        MobileAds.initialize(this) {}
+//AdMob: Banner
+        mAdView = findViewById(R.id.adViewBanner)
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
     }
 }
