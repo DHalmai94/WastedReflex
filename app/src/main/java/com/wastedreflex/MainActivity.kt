1aacc4433bce25d6af522959c0875d35a9113cf4
MaKiVerem4011 <gergo.tallai@gmail.com>
2020-08-11 18:03:30 +0200

    Banner reklám TestID-val működik. AndroidManifest.xml-be és a layout-on kell az ID változtatni és lehet tesztelni élesbe telefonon Képek, Layout, random szám generálás, 2 activity közötti átjárás

--- app/src/main/java/com/wastedreflex/MainActivity.kt
+++ app/src/main/java/com/wastedreflex/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.wastedreflex
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.gms.ads.MobileAds
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,5 +21,10 @@ class MainActivity : AppCompatActivity() {
         mAdView = findViewById(R.id.adViewBanner)
         val adRequest = AdRequest.Builder().build()
         mAdView.loadAd(adRequest)
+
+        newGameBtn.setOnClickListener {
+            val intent = Intent(applicationContext,single_playground_2x2::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/wastedreflex/single_playground_2x2.kt
@@ -0,0 +1,25 @@
+package com.wastedreflex
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_single_playground_2x2.*
+import kotlin.random.Random
+
+class single_playground_2x2 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_single_playground_2x2)
+
+
+
+        generateBtn.setOnClickListener {
+            val randomBlock = (1..4).random()
+            println(randomBlock)
+            Toast.makeText(this@single_playground_2x2, randomBlock.toString(), Toast.LENGTH_SHORT).show()
+        }
+    }
+}
